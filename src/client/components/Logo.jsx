e0975f632b9aed58ec021ff680f5bd477887ab2d
Volha Paddubka <Volha_Paddubka@epam.com>
2018-05-21 17:56:19 +0300

    returned inline styles for now

--- src/client/components/Logo.jsx
+++ src/client/components/Logo.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import './logo.css';
+//import './logo.css';
 
 const Logo = props =>
-  <div className='logo'>netflixroulette</div>;
+  <div className='logo'>netflixroulette
+    <style jsx>{`
+      .logo {
+        color: #e75f77;
+        margin-bottom: 60px;
+      }
+    `}</style>
+  </div>;
 
 export default Logo;
